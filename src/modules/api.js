4f9df68a0f1c49241f41847903c54c7c698bf34e
Yasmin Elmahdi <elmahdiyasmin@.com>
2023-09-18 13:51:20 +0300

    Fix api.js issue

--- src/modules/api.js
+++ src/modules/api.js
@@ -22,17 +22,17 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
   },
   airPollution(lat,lon){
-    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`;
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}&units=metric`;
   },
   reverseGeo(lat,lon){
-    return `http://api.openweathermap.org/geo/2.5/reverse?${lat}&${lon}&limit=5`;
+    return `https://api.openweathermap.org/geo/2.5/reverse?${lat}&${lon}&limit=5`;
   },
   /**
    * 
    * @param {string} query search query e.g: 'London 
    */
   geo(query){
-    return `http://api.openweathermap.org/geo/2.5/direct?q=${query}&limit=5`;
+    return `https://api.openweathermap.org/geo/2.5/direct?q=${query}&limit=5`;
   }
 
 };
